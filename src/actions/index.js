b92354450e57f81643dadf2ae2955539ab3c4989
Burhan Ali <burhan.ali@graze.com>
2018-01-14 23:13:14 +0000

    Get api key from environment

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// Not a personal API key. Taken from the public example given on the openweather website.
-const API_KEY = 'b6907d289e10d714a6e88b30761fae22';
+// Pull in the key from the environment at the time of bundling
+const API_KEY = WEATHER_API_KEY;
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
